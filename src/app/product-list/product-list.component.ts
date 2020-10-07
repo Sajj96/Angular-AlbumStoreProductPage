e56e4f6c3dcf09ea9db02b4ce009baf29b013dc5
Sajj96 <captainsajjad4@gmail.com>
2020-10-07 09:46:04 +0300

    Added product list component

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'app/product.service';
+import { ProductService } from '../product.service';
 import { Product } from '../product';
 
 @Component({
@@ -14,7 +14,7 @@ export class ProductListComponent implements OnInit {
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getProducts().subscribe(response => this.products = response);
+    this._productService.getProducts().subscribe(response => this.products = response );
   }
 
 }
